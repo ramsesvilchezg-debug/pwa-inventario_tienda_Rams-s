74ea1db8be5a87afafee9dc045c2bcc97104858f
ramsesvilchezg-debug <ramses.vilchezg@uniagustiniana.edu.co>
2026-08-20 09:04:52 -0500

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "ramses-inventario-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+// Instalar y guardar en caché
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activar y limpiar cachés viejas
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptar peticiones y servir desde caché
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
